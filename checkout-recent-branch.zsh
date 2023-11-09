14884d9833ea3a12954d6547433f7114fa9ac99f
Zacharyjlo <zac@hospitalityonline.com>
2023-11-09 12:23:56 -0800

    allow number of branches to be selected

--- checkout-recent-branch.zsh
+++ checkout-recent-branch.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 function get_recent_branches() {
+  local count=${1:-10}  # Use the first argument if provided, otherwise default to 10
   local merged_branches=$(git for-each-ref --merged main refs/heads/ --format '%(refname:short)')
   local all_branches=$(git branch --list | tr -d ' ')
   local recent_branches=()
@@ -11,22 +12,27 @@ function get_recent_branches() {
       fi
     fi
   done < <(git reflog show --oneline -n 500 | grep 'checkout: moving' | awk '{print $NF}' | tr -d ' ')
-  echo "${recent_branches[@]:0:10}"
+  echo "${recent_branches[@]:0:$count}"
 }
 
-local branches=($(get_recent_branches))
+# Usage: rbr [number]
+
+# Get the number of recent branches to show from the first script argument
+# If no argument is provided, it defaults to 10
+local branches=($(get_recent_branches "$1"))
 local length=${#branches[@]}
 echo "\n\e[1;34mSelect a branch by number:\e[0m"
 echo "\e[1;32m─────────────────────────────\e[0m"
-for ((i=1; i<=$length; i++)); do
-  echo "\e[1;36m$((i)).\e[0m ${branches[$i]}"
+for ((i=0; i<$length; i++)); do
+  echo "\e[1;36m$((i+1)).\e[0m ${branches[$i]}"
 done
 echo "\e[1;32m─────────────────────────────\e[0m"
 print -n "\e[1;33mEnter the branch number:\e[0m "
 read branch_num
 if ((branch_num >= 1 && branch_num <= length)); then
-  git checkout "${branches[$((branch_num))]}"
+  git checkout "${branches[$((branch_num-1))]}"
 else
   echo "\e[1;31mInvalid selection.\e[0m"
 fi
 echo "\e[0m"
+
