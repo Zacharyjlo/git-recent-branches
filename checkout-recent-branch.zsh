a46eecc1194f39b60758d5c72181091b04b484eb
Zacharyjlo <zac@hospitalityonline.com>
2023-11-09 12:30:04 -0800

    try again

--- checkout-recent-branch.zsh
+++ checkout-recent-branch.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 function get_recent_branches() {
-  local count=${1:-10}  # Use the first argument if provided, otherwise default to 10
+  local count=$1  # This will take the first argument passed to the function
   local merged_branches=$(git for-each-ref --merged main refs/heads/ --format '%(refname:short)')
   local all_branches=$(git branch --list | tr -d ' ')
   local recent_branches=()
@@ -15,12 +15,15 @@ function get_recent_branches() {
   echo "${recent_branches[@]:0:$count}"
 }
 
-# Usage: rbr [number]
+# Now you can specify the number of branches when you call the function
+# If no number is provided, default to showing 10 branches
+if [ -z "$1" ]; then
+  branches=($(get_recent_branches 10))
+else
+  branches=($(get_recent_branches "$1"))
+fi
 
-# Get the number of recent branches to show from the first script argument
-# If no argument is provided, it defaults to 10
-local branches=($(get_recent_branches "$1"))
-local length=${#branches[@]}
+length=${#branches[@]}
 echo "\n\e[1;34mSelect a branch by number:\e[0m"
 echo "\e[1;32m─────────────────────────────\e[0m"
 for ((i=1; i<=$length; i++)); do
@@ -35,4 +38,3 @@ else
   echo "\e[1;31mInvalid selection.\e[0m"
 fi
 echo "\e[0m"
-
