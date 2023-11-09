1abd443c6413cf196f366376ccf71a18e09635a7
Zacharyjlo <zac@hospitalityonline.com>
2023-11-09 13:06:21 -0800

    test

--- checkout-recent-branch.zsh
+++ checkout-recent-branch.zsh
@@ -23,6 +23,7 @@ display_count=$(( length < branch_limit ? length : branch_limit ))
 local branches=($(get_recent_branches "$1"))
 echo "\n\e[1;34mSelect a branch by number:\e[0m"
 echo "\e[1;32m─────────────────────────────\e[0m"
+echo "$display_count"
 for ((i=1; i<=$display_count; i++)); do
   echo "\e[1;36m$((i)).\e[0m ${branches[$i]}"
 done
